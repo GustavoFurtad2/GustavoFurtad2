e701be2a81672b555e6af5b3b114c3db86616e77
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2025-05-31 23:53:32 -0300

    update context

--- obj/Debug/net6.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net6.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de00f0bdf1fec4d341dc7f15ed354f5cee736f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91db4922de12ec60cf5cab5d26629ab802bdae59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
